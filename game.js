a3a004af7c9800219d52d468a356dc6e2eed9ab5
AaronW <zmtzwb@gmail.com>
2019-01-30 18:20:04 +0800

    little change

--- game.js
+++ game.js
@@ -3,13 +3,14 @@ import Scene from "./lib/Scene";
 import Color from './lib/Utils';
 
 // import Aircraft from './components/Aircraft'
-import Aircraft from './samples/aircraft/Aircraft';
+// import Aircraft from './samples/aircraft/Aircraft';
 import Player from './components/Player';
 
 import './lib/weapp-adapter'
 
 
 import Runtime from './lib/Runtime'
+import Aircraft from "./samples/aircraft/Aircraft";
 
 // let canvas = wx.createCanvas();
 let context = canvas.getContext('2d');
@@ -22,10 +23,23 @@ let p1 = new Player(context);
 p1.x = 50;
 p1.y = 50;
 scene.addGameObject(p1)
-
-
 runtime.addScene(scene);
-runtime.changeScene(0)
+
+let scene2 = new Scene(context);
+scene2.addGameObject(new Aircraft(context));
+runtime.addScene(scene2);
+
+let id = 0;
+setInterval(function () {
+    if (id === 0) {
+        runtime.changeScene(1);
+        id = 1;
+    } else {
+        runtime.changeScene(0);
+        id = 0;
+    }
+
+}, 3000);
 
 // EventBus.bind('update', function (msg) {
 //     // console.log("ok event, msg is %s", msg)
